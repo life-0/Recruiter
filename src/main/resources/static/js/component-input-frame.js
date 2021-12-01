2dc584364cb6a59b2ab07735390bdea78f8d652e
life-0 <1471925853@qq.com>
2021-12-01 19:50:59 +0800

    解决动态输入框输入不可修改的参数

--- src/main/resources/static/js/component-input-frame.js
+++ src/main/resources/static/js/component-input-frame.js
@@ -2,37 +2,23 @@
     $(document).ready(function () {
         /*FromFrame 传值框架*/
 
-        /*动态输入框*/
-        let field = [];    /*存储列的值*/
-        let theadValues = $(".table").children('thead').find('tr').children('th');
-        theadValues.each(function (key, value) {
-            /*console.log(key + " : " + $(value).html())*/
-            let json = {};
-            if ($(value).attr('readOnly') === 'true') {
-                json.name = $(value).html();
-                json.readOnly = "true";
-            } else {
-                json.name = $(value).html();
-                json.readOnly = "false";
-            }
-            field.push(json);
-        })
-        field.shift();   //去掉头部键值对参数(全选)
-        field.pop();     //去掉尾部参数(操作)
+        function dynamicInputBox(data, subject) {
+            //动态加载,当前数据,当前对象
 
-        function dynamicInputBox(data) {    //动态加载
-            let inputFrame = $(".input-frame");   //获取输入框类
-            inputFrame.css("height", 43 * (data.length + 2) + 'px!important');//动态扩容框架
             for (let js in data) {
                 // console.log(js + ": " + data[js].name + " " + data[js].readOnly)
                 /*'<p>'+data[js].name+'</p>'*/
-                inputFrame.find(".operate").before(
-                    '<div class="form-group" style="flex-wrap: nowrap;display:flex;">' +
+                let html = '<div class="form-group" style="flex-wrap: nowrap;display:flex;">' +
                     '<label style="width: 10%;line-height: 34px;font-size: 10px;margin-right: 5px">' +
                     data[js].name + ":" +
                     '</label>' +
-                    '<input type="text" name="id" class="form-control">' +
-                    '</div>'
+                    '<input type="text" name="id" class="form-control" ' +
+                    'value="' + data[js].value + '"';
+                if (data[js].readonly) {
+                    html += 'readonly="' + data[js].readOnly + '">';
+                }
+                subject.find(".operate").before(
+                    html + '</div>'
                 )
             }
 
@@ -43,11 +29,35 @@
             $(".overlap").css("display", 'none');
         });
         $(".test").on('click', function () {
-            let input_frame = $(".input-frame");
-            input_frame.css('display', "block");
-            input_frame.css("height", 55 * (field.length + 2) + 'px');
+            /*动态输入框*/
+            let field = [];    /*存储列的值*/
+            let theadValues = $(".table").children('thead').find('tr').children('th');/*获取展示表的字段*/
+            let input_frame = $(".input-frame");    /*获取输入框类*/
+            let updateObjectArray = $(this).parents('td').siblings();    //获取值
+            updateObjectArray.push($(this));    //添加自身,补齐个数
+            console.log(updateObjectArray)
+            theadValues.each(function (key, value) {
+                /*console.log(key + " : " + $(value).html())*/
+                let json = {};
+                json.name = $(value).html();
+                console.log()
+                if (updateObjectArray[key].className === "updateInvalid") {
+                    json.readonly = true;
+                }
+
+                if (updateObjectArray[key].innerText)
+                    json.value = updateObjectArray[key].innerText;
+                // console.log(json.value)
+                field.push(json);   //装入数据
+            })
+            field.shift();   //去掉头部键值对参数(全选)
+            field.pop();     //去掉尾部参数(操作)
+
+            //装填数据后再进行修输入框样式
+            input_frame.css('display', "block");    //修改其可展示状态
+            input_frame.css("height", 55 * (field.length + 2) + 'px');  //修改动态扩容其高度
             $(".overlap").css("display", 'block');
-            dynamicInputBox(field);
+            dynamicInputBox(field, input_frame);
         });
     });
 })(jQuery);
